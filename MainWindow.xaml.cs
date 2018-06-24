6b62c8d5bb58619a77a21e1a6588a9ec547f7874
Aditya Singh <adityasinghwolvieait@gmail.com>
2018-06-24 20:33:57 +0530

    Issue- Check button resolved!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,123 +1,142 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace FileScramblerV1._0
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private Microsoft.Win32.OpenFileDialog rx_dlg;
-        private Microsoft.Win32.OpenFileDialog tx_dlg;
+        // OpenFileDialog box for input file and FolderBrowserDialog box for output file
+        private Microsoft.Win32.OpenFileDialog input_dlg;
+        private FolderBrowserDialog output_dlg;
+        
+        private string input_FileLocation;
+        private string output_FileLocation;
 
-        private string rx_FileLocation;
-        private string tx_FileLocation;
+        private string input_FileName;
 
         public MainWindow()
         {
             //InitializeComponent();
-            
         }
 
+        // Click event function for browse button 1
         private void BrowserButton1_Click(object sender, RoutedEventArgs e)
         {
-            // Create Open FileDialog
-            rx_dlg = new Microsoft.Win32.OpenFileDialog();
+            // Create Open File Dialog object
+            input_dlg = new Microsoft.Win32.OpenFileDialog();
+            
+            /*
+            To specify the file type for reception
+            input_dlg.DefaultExt = ".txt";
+            input_dlg.Filter = "Text documents (.txt)|*.txt";
+            */
 
-            //rx_dlg.DefaultExt = ".txt";
-            //rx_dlg.Filter = "Text documents (.txt)|*.txt";
+            // Display Open File Dialog box
+            Nullable<bool> result = input_dlg.ShowDialog();
 
-            // Display Open File Dialog
-            // result is declared implict 'aive hi'
-            // Nullable<bool> result = rx_dlg.ShowDialog();
-            var result = rx_dlg.ShowDialog();
-
+            // If the user has selected some file
             if (result == true)
             {
-                rx_FileLocation = rx_dlg.FileName;
-                FilenameTextBox1.Text = rx_FileLocation;
+                // Collect file FULL PATH location
+                input_FileLocation = input_dlg.FileName;
+                FilenameTextBox1.Text = input_FileLocation;
+
+                // Collect file NAME
+                input_FileName = input_dlg.SafeFileName;
             }
         }
 
+        // Click event function for browser button 2
         private void BrowseButton2_Click(object sender, RoutedEventArgs e)
         {
-            // Create Open FileDialog
-            tx_dlg = new Microsoft.Win32.OpenFileDialog();
+            // Create an object of FolderBrowserDialog
+            output_dlg = new FolderBrowserDialog();
 
-            //tx_dlg.DefaultExt = ".txt";
-            //tx_dlg.Filter = "Text documents (.txt)|*.txt*";
+            // Open the dialog box
+            DialogResult result = output_dlg.ShowDialog();
 
-            // Display Open File Dialog
-            // result is declared implict 'aive hi'
-            // Nullable<bool> result = rx_dlg.ShowDialog();
-            var result = tx_dlg.ShowDialog();
-
-            if(result==true)
+            if(result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(output_dlg.SelectedPath))
             {
-                // Get the filename from dialog box
-                FilenameTextBox2.Text = tx_dlg.FileName;
-
-                // store the user updated filename
-                tx_FileLocation = FilenameTextBox2.Text;
+                // Append the filename to selected path by user
+                output_FileLocation = output_dlg.SelectedPath + "\\Conv_" + input_FileName;
+                FilenameTextBox2.Text = output_FileLocation;
             }
         }
 
+        // Checked event function for check box
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             FilenameTextBox2.Clear();
-            FilenameTextBox2.Text = FilenameTextBox1.Text;
-            tx_FileLocation = FilenameTextBox2.Text;
 
             BrowseButton2.Visibility = Visibility.Hidden;
+
+            // input_FileLocation contains the full path to input file
+            // next we insert "Conv_" word in between "C:\----Filename.cs"
+
+            char[] temp_ar = input_FileLocation.ToCharArray();
+            
+            // last occurance of '\' in file path location
+            int last_occ = 0;
+            for (int i = temp_ar.Length-1; i>=0; i--)
+                if(temp_ar[i]=='\\')
+                {
+                    last_occ = i;
+                    break;
+                }
+            char[] temp_ar2 = new char[last_occ+1];
+            for (int i = 0; i <= last_occ; i++)
+                temp_ar2[i] = temp_ar[i];
+            string s = new string(temp_ar2) + "Conv_" + input_FileName;
+
+            output_FileLocation = s;
+            FilenameTextBox2.Text = output_FileLocation;
         }
 
+        // Click event function for convert button
         private void ConvertButton_Click(object sender, RoutedEventArgs e)
         {
-            // The below 2 lines of code will work too. 
-            // Copy file loc. from textbox when convert button is pressed
-            rx_FileLocation = FilenameTextBox1.Text;
-            tx_FileLocation = FilenameTextBox2.Text;
-
             try
             {
-                string text = System.IO.File.ReadAllText(@rx_FileLocation);
+                // Import the whole file contents from input file @input_FileLocation
+                string rx_FileContent = System.IO.File.ReadAllText(@input_FileLocation);
 
-                char[] ar = text.ToCharArray();
-                
-                for(int i = 0;i<ar.Length;i++)
+                // print the no. of lines on console. (Just fun!)
+                int count_NewLines = CountNewLines(rx_FileContent);
+                Console.WriteLine("No. of code lines: " + count_NewLines);
+
+                // Encode the contents of file by EX-OR opertions
+                char[] rx_FileContentArray = rx_FileContent.ToCharArray();
+
+                for(int i = 0;i<rx_FileContentArray.Length;i++)
                 {
-                    if (ar[i] != '\n')
-                        ar[i] = (char)((int)ar[i] ^ 2);
+                    if (rx_FileContentArray[i] != '\n')
+                        rx_FileContentArray[i] = (char)((int)rx_FileContentArray[i] ^ 1);
                     else
-                        ar[i] = Environment.NewLine.ToCharArray()[0] ;
+                        rx_FileContentArray[i] = Environment.NewLine.ToCharArray()[0];
                 }
 
-                string newS = new string(ar);
+                // Create a new file by inserting new_FileContent (Encoded) data.
+                string new_FileContent = new string(rx_FileContentArray);
 
-                Console.WriteLine(newS);
-
-                System.IO.File.WriteAllText(@tx_FileLocation, newS);
+                System.IO.File.WriteAllText(@output_FileLocation, new_FileContent);
             }
             catch (Exception err)
             {
-                Console.WriteLine(err);
+                Console.Write(err);
             }
-            
 
+            System.Windows.Application.Current.Shutdown();
+        }
+        private int CountNewLines(string content)
+        {
+            int temp_CountNewLine = 0;
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (content.ToCharArray()[i] == '\n')
+                    temp_CountNewLine++;
+            }
+            return temp_CountNewLine;
         }
     }
 }
